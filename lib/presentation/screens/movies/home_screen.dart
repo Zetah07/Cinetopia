63988567135430b51fc5ca4afcf0485958fa73b0
Zetah07 <sebastiancastro0792@gmail.com>
2023-08-14 11:49:53 -0500

    go router - tabs

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
-import 'package:intl/date_symbol_data_local.dart';
-
-import 'package:cinetopia/presentation/providers/providers.dart';
 import 'package:cinetopia/presentation/widgets/widgets.dart';
 
 
@@ -11,115 +6,21 @@ import 'package:cinetopia/presentation/widgets/widgets.dart';
 class HomeScreen extends StatelessWidget {
 
   static const name = 'home-screen';
-  
 
-  const HomeScreen({super.key});
+  final Widget childView;
+
+
+  const HomeScreen({
+    super.key, 
+    required this.childView,
+    // required int pageIndex
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-
-  @override
-  void initState() {
-    super.initState();
-    
-    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
-    ref.read( popularMoviesProvider.notifier ).loadNextPage();
-    ref.read( topRatedMoviesProvider.notifier ).loadNextPage();
-    ref.read( upcomingMoviesProvider.notifier ).loadNextPage();
-  }
-
-
-  @override
-    Widget build(BuildContext context) {
-      
-      initializeDateFormatting('es');
-      DateTime now = DateTime.now();
-      String formattedDate = DateFormat('EEEE d', 'es').format(now);
-
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if ( initialLoading ) return const FullScreenLoader();
-    
-    final slideShowMovies = ref.watch( moviesSlideshowProvider );
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
-    final popularMovies = ref.watch( popularMoviesProvider );
-    final topRatedMovies = ref.watch( topRatedMoviesProvider );
-    final upcomingMovies = ref.watch( upcomingMoviesProvider );
-
-    return CustomScrollView(
-      slivers: [
-
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppbar(),
-            titlePadding: EdgeInsets.all(5),
-          ),
-        ),
-
-
-        SliverList(delegate: SliverChildBuilderDelegate(
-          (context, index) {
-              return Column(
-                  children: [
-              
-                    // const CustomAppbar(),
-              
-                    MoviesSlideshow(movies: slideShowMovies ),
-              
-                    MovieHorizontalListview(
-                      movies: nowPlayingMovies,
-                      title: 'En cines',
-                      subTitle: 'hoy: $formattedDate',
-                      loadNextPage: () =>ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
-                      
-                    ),
-              
-                    MovieHorizontalListview(
-                      movies: upcomingMovies,
-                      title: 'Próximamente',
-                      subTitle: 'En este mes',
-                      loadNextPage: () =>ref.read(upcomingMoviesProvider.notifier).loadNextPage()
-                    ),
-              
-                    MovieHorizontalListview(
-                      movies: popularMovies,
-                      title: 'Populares',
-                      // subTitle: '',
-                      loadNextPage: () =>ref.read(popularMoviesProvider.notifier).loadNextPage()
-                    ),
-              
-                    MovieHorizontalListview(
-                      movies: topRatedMovies,
-                      title: 'Mejor calificadas',
-                      subTitle: 'Desde siempre',
-                      loadNextPage: () =>ref.read(topRatedMoviesProvider.notifier).loadNextPage()
-                    ),
-
-                    const SizedBox( height: 10 ),
-              
-              
-                  ],
-                );
-          },
-          childCount: 1
-        )),
-
-      ]
+    return Scaffold(
+      body: childView,
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
 }
